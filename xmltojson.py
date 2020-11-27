69e39592f1d004a9a9839f70656711008d1ce09f
Konstantin Gerastenok <frankgover@rambler.ru>
2020-11-27 23:33:01 +0300

    Update xmltojson.py

--- xmltojson.py
+++ xmltojson.py
@@ -5,47 +5,47 @@ def parse(element):
 
     if((type(element) != type(tuple()))):
         for item in element.items():
-            if((type(result.get(str("@" + xml.QName(item[0]).localname).strip())) == type(None))):
-                result[str("@" + xml.QName(item[0]).localname).strip()] = dict()
+            if((type(result.get(str("@" + xml.QName(item[0]).text).strip())) == type(None))):
+                result[str("@" + xml.QName(item[0]).text).strip()] = list()
             if((type(item) != type(None))):
-                result.get(str("@" + xml.QName(item[0]).localname).strip()).update(parse(item))
+                result.get(str("@" + xml.QName(item[0]).text).strip()).append(parse(item))
     if((type(element) != type(None))):
         if((type(element) != type(tuple()))):
-            if((type(result.get(str("#" + "name").strip())) == type(None))):
-                result[str("#" + "name").strip()] = str()
+            if((type(result.get(str("%" + "name").strip())) == type(None))):
+                result[str("%" + "name").strip()] = str()
             if((type(xml.QName(element.tag).localname) != type(None))):
-                result[str("#" + "name").strip()] += str(xml.QName(element.tag).localname).strip()
-            if((type(result.get(str("#" + "text").strip())) == type(None))):
-                result[str("#" + "text").strip()] = str()
+                result[str("%" + "name").strip()] += str(xml.QName(element.tag).localname).strip()
+            if((type(result.get(str("%" + "text").strip())) == type(None))):
+                result[str("%" + "text").strip()] = str()
             if((type(element.text) != type(None))):
-                result[str("#" + "text").strip()] += str(element.text).strip()
+                result[str("%" + "text").strip()] += str(element.text).strip()
             if((type(element.tail) != type(None))):
-                result[str("#" + "text").strip()] += str(element.tail).strip()
-            if((type(result.get(str("#" + "xmlns").strip())) == type(None))):
-                result[str("#" + "xmlns").strip()] = str()
+                result[str("%" + "text").strip()] += str(element.tail).strip()
+            if((type(result.get(str("%" + "xmlns").strip())) == type(None))):
+                result[str("%" + "xmlns").strip()] = str()
             if((type(xml.QName(element.tag).namespace) != type(None))):
-                result[str("#" + "xmlns").strip()] += str(xml.QName(element.tag).namespace).strip()
+                result[str("%" + "xmlns").strip()] += str(xml.QName(element.tag).namespace).strip()
         else:
-            if((type(result.get(str("#" + "name").strip())) == type(None))):
-                result[str("#" + "name").strip()] = str()
+            if((type(result.get(str("%" + "name").strip())) == type(None))):
+                result[str("%" + "name").strip()] = str()
             if((type(xml.QName(element[0]).localname) != type(None))):
-                result[str("#" + "name").strip()] += str(xml.QName(element[0]).localname).strip()
-            if((type(result.get(str("#" + "text").strip())) == type(None))):
-                result[str("#" + "text").strip()] = str()
+                result[str("%" + "name").strip()] += str(xml.QName(element[0]).localname).strip()
+            if((type(result.get(str("%" + "text").strip())) == type(None))):
+                result[str("%" + "text").strip()] = str()
             if((type(element[1]) != type(None))):
-                result[str("#" + "text").strip()] += str(element[1]).strip()
+                result[str("%" + "text").strip()] += str(element[1]).strip()
             if((type(element[1]) != type(None))):
-                result[str("#" + "text").strip()] += str(          ).strip()
+                result[str("%" + "text").strip()] += str(          ).strip()
             if((type(result.get(str("#" + "xmlns").strip())) == type(None))):
-                result[str("#" + "xmlns").strip()] = str()
+                result[str("%" + "xmlns").strip()] = str()
             if((type(xml.QName(element[0]).namespace) != type(None))):
-                result[str("#" + "xmlns").strip()] += str(xml.QName(element[0]).namespace).strip()
+                result[str("%" + "xmlns").strip()] += str(xml.QName(element[0]).namespace).strip()
     if((type(element) != type(tuple()))):
         for item in element.getchildren():
-            if((type(result.get(str(" " + xml.QName(item.tag).localname).strip())) == type(None))):
-                result[str(" " + xml.QName(item.tag).localname).strip()] = list()
+            if((type(result.get(str("#" + xml.QName(item.tag).text).strip())) == type(None))):
+                result[str("#" + xml.QName(item.tag).text).strip()] = list()
             if((type(item) != type(None))):
-                result.get(str(" " + xml.QName(item.tag).localname).strip()).append(parse(item))
+                result.get(str("#" + xml.QName(item.tag).text).strip()).append(parse(item))
 
     return(result)
 
